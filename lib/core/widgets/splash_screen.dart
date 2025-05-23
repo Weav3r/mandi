c05326f97865ad11c2e4138109ad2fdab15b07d5
Ernest <66331153+Weav3r@users.noreply.github.com>
2025-05-23 08:26:01 +0000

    add background color

--- lib/core/widgets/splash_screen.dart
+++ lib/core/widgets/splash_screen.dart
@@ -5,6 +5,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.green,
       body: Center(child: CircularProgressIndicator()),
     );
   }
